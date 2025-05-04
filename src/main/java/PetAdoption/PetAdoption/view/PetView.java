c278c3b23b99d4d35f660a1f3254332eb7324c62
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-05-04 13:22:48 -0500

    added imports and proper brackets in PetView.java

--- src/main/java/PetAdoption/PetAdoption/view/PetView.java
+++ src/main/java/PetAdoption/PetAdoption/view/PetView.java
@@ -1,12 +1,43 @@
 package PetAdoption.PetAdoption.view;
 
-public class PetView {
+import PetAdoption.PetAdoption.model.Pet;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
 
-}
+public class PetView extends JFrame {
+	private JTable petTable;
+	private DefaultTableModel tableModel;
+	private JButton adoptButton;
+	private JButton removeButton;
+	private JButton refreshButton;
+	private JComboBox<String> sortBox;
+	
+	public PetView() {
 
-public JButton getAdoptButton();
-public JButton getRemoveButton();
-public JButton getRefreshButton();
-public JComboBox<String> getSortBox();
-public Pet getSelectedPet();
-public void updatePetTable(List<pet> pets);
+	}
+
+	public JButton getAdoptButton() {
+	
+	}
+	
+	public JButton getRemoveButton() {
+		
+	}
+	
+	public JButton getRefreshButton() {
+		
+	}
+	
+	public JComboBox<String> getSortBox(){
+		
+	}
+	
+	public Pet getSelectedPet() {
+		
+	}
+	
+	public void updatePetTable(List<Pet> pets) {
+		
+	}
