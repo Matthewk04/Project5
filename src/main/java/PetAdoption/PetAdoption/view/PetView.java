fb79ac41c6c2804ad01decebc1dca7a49aa76c24
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-05-04 12:59:49 -0500

    added notes to PetView.java to finish later

--- src/main/java/PetAdoption/PetAdoption/view/PetView.java
+++ src/main/java/PetAdoption/PetAdoption/view/PetView.java
@@ -3,3 +3,10 @@ package PetAdoption.PetAdoption.view;
 public class PetView {
 
 }
+
+public JButton getAdoptButton();
+public JButton getRemoveButton();
+public JButton getRefreshButton();
+public JComboBox<String> getSortBox();
+public Pet getSelectedPet();
+public void updatePetTable(List<pet> pets);
