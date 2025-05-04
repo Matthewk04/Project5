5e9cfda4b9340c7710d919a7be62169f9c8fe165
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-05-04 12:50:34 -0500

    created general layout for PetController.java

--- src/main/java/PetAdoption/PetAdoption/controller/PetController.java
+++ src/main/java/PetAdoption/PetAdoption/controller/PetController.java
@@ -1,5 +1,43 @@
 package PetAdoption.PetAdoption.controller;
 
-public class PetController {
+import PetAdoption.PetAdoption.model.Shelter;
+import PetAdoption.PetAdoption.model.Pet;
+import PetAdoption.PetAdoption.model.utils.comparators.AgeComparator;
+import PetAdoption.PetAdoption.model.utils.comparators.NameComparator;
+import PetAdoption.PetAdoption.model.utils.comparators.SpeciesComparator;
+import PetAdoption.PetAdoption.view.PetView;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class PetController {
+	private Shelter shelter;
+	private PetView view;
+	
+	public PetController(Shelter shelter, PetView view) {
+		
+	}
+	
+	private void initController() {
+		
+	}
+	
+	private void refreshPetList() {
+		
+	}
+	
+	private void adoptSelectedPet() {
+		
+	}
+	
+	private void removeSelectedPet() {
+		
+	}
+	
+	private void sortPets() {
+		
+	}
 }
